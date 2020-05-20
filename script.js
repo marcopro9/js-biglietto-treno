1607cb337b1653355646c42170e2f565065b1d18
Marco <Prosperi>
2020-05-20 16:32:34 +0200

    esercizio finito

---
+++ script.js
@@ -0,0 +1,19 @@
+// variables
+var totalKm = parseInt( prompt("quanti km devi percorrere in totale?", ""));
+var passengerAge = parseInt( prompt("quanti anni hai?"));
+var regularPrice = (totalKm * 0.21);
+var juniorPrice = regularPrice - ((regularPrice * 20) /100);
+var seniorPrice = regularPrice - ((regularPrice * 40) /100);
+var ticketPrice = regularPrice
+
+// conditions
+if ( passengerAge > 64 ){
+  ticketPrice = seniorPrice;
+}
+else if( passengerAge < 18 ){
+  ticketPrice = juniorPrice;
+}
+
+// result in HTML
+document.getElementById('price').innerHTML =
+"il prezzo del biglietto è " + ticketPrice.toFixed(2) + " €";
